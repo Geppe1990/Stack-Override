4a2dbfd83c978fc2684a20e2ea873aa7ce1c4e54
Giuseppe Vigneri <geppe1990@gmail.com>
2021-01-14 22:55:49 +0100

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -5,28 +5,37 @@ let acceptedAnswer = acceptedAnswer();
 function getAcceptedAnswer() {
     var definitiveAnswer = document.querySelector('.accepted-answer') ? document.querySelector('.accepted-answer') : null;
 
-    if (definitiveAnswer) {
+    if (definitiveAnswer) { // SE PRESENTE RISPOSTA CON SPUNTA
         return definitiveAnswer
     } else {
-        let answers = document.querySelectorAll('.answer');
-        let highestVote = 0;
+        return getAlternativeAnswer();
+    }
+}
 
+function getAlternativeAnswer() { // SE NON PRESENTE RISPOSTA CON SPUNTA
+    let answers = document.querySelectorAll('.answer');
+    let highestVote = 0;
+    let errorMessage = "No valid answer";
+
+    if(answers) { // SE PRESENTE ALMENO UNA RISPOSTA QUALSIASI
         answers.forEach(answer => {
             var vote = parseInt(answer.querySelector("[itemprop='upvoteCount']").innerText);
-
+    
             if (vote > highestVote) {
                 highestVote = vote;
                 definitiveAnswer = answer
             }
         });
-
-        if (definitiveAnswer) {
+    
+        if (definitiveAnswer) { // SE HO UNA RISPOSTA VALIDA CON VOTO PIU ALTO
             return definitiveAnswer
         } else {
-            return "No valid answer"
+            return errorMessage; // SE NON HO UNA RISPOSTA VALIDA
         }
+    } else { // SE NON PRESENTE NESSUNA RISPOSTA
+        return errorMessage;
     }
 }
 
-console.log(highestVote);
-console.log(acceptedAnswer);
+console.log(highestVote);    // ONLY FOR DEBUG PORPOSE
+console.log(acceptedAnswer); // ONLY FOR DEBUG PORPOSE
