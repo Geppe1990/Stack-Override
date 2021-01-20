e73eb381101a1fa15fb766fd367e91a9845ec203
Giuseppe Vigneri <geppe1990@gmail.com>
2021-01-20 21:10:35 +0100

    Deploy command

--- gulpfile.js
+++ gulpfile.js
@@ -47,4 +47,5 @@ gulp.task('watch', function () {
 	gulp.watch('./src/scss/*.scss', gulp.series('sass'));	
 });
 
+gulp.task('build', gulp.series('lintJS', 'uglifyJS', 'sass'));
 gulp.task('default', gulp.series('lintJS', 'uglifyJS', 'sass', 'watch'));
